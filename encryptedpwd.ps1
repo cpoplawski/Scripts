6507802ab44a3a2ca58e4e5e56fe2ea8a328488e
cpoplawski <c.poplawski.vivivi@gmail.com>
2014-04-22 11:07:07 -0400

    Created encrypted password file

---
+++ encryptedpwd.ps1
@@ -0,0 +1,4 @@
+﻿<# Set and encrypt credentials to file using default method #>
+ 
+$credential = Get-Credential
+$credential.Password | ConvertFrom-SecureString | Set-Content "c:\Users\chris.poplawski\Desktop\voice enable\lyncserviceCPFEpwd.txt" 
